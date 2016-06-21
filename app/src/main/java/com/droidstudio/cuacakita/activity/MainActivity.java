6d1865d175e15237bdac80c4076a16cfa03cb6ef
ferysyukur <ferysyukur26@gmail.com>
2016-06-21 14:37:38 +0800

    CuacaKita v.0.0.1

---
+++ app/src/main/java/com/droidstudio/cuacakita/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.droidstudio.cuacakita.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.droidstudio.cuacakita.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
